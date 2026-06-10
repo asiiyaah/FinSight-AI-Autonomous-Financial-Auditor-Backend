e6934b831fa5796b51b4ec87dc49c8dddcf50bb1
asiiyaah <asiyamsali16@gmail.com>
2026-06-10 11:14:11 +0530

    add statements app with statement and transaction models

--- finsight/settings.py
+++ finsight/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'accounts',
+    'statements',
 ]
 
 MIDDLEWARE = [
---
+++ statements/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Statement(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    file=models.FileField(upload_to='statements/')
+    file_name = models.CharField(max_length=255)
+    file_type = models.CharField(max_length=10)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    is_parsed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.file_name}"
+    
+class Transaction(models.Model):
+    statement = models.ForeignKey(Statement, on_delete=models.CASCADE, related_name='transactions')
+    date = models.DateField()
+    vendor = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.date} - {self.vendor} - {self.amount}"
+    
